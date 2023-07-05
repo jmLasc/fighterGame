14f9b77cbeb3f94eb785ff5820bf797870fd4088
jmLascano <jm.lascano2@gmail.com>
2023-07-05 01:47:47 -0400

    assets folder and fighter class

---
+++ fighter.py
@@ -0,0 +1,8 @@
+import pygame
+
+class Character():
+    def __init__(self, x, y):
+        self.rect = pygame.Rect(x, y, 80, 180)
+    
+    def draw(self, surface):
+        pygame.draw.rect(surface, (255, 0, 0),  )
