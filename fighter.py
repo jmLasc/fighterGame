fe2fcf9878a3e1abac3bf38a4510ac1fa2d0fcdb
jmLascano <jm.lascano2@gmail.com>
2023-07-05 04:46:50 -0400

    Better resembles an actual fighting game - Movement - HP Bars - Attack concept - Flipping

--- fighter.py
+++ fighter.py
@@ -2,7 +2,72 @@ import pygame
 
 class Character():
     def __init__(self, x, y):
+        self.leftFacing = False
         self.rect = pygame.Rect(x, y, 80, 180)
-    
+        self.health = 100
+        self.vel_y = 0
+        self.jump = False
+        self.attacking = False
+        self.attack_type = 0
+        
+    def move(self, screen_width, screen_height, surface, target): # TODO
+        MOVSPEED = 10
+        GRAV = 3 
+        dx = 0
+        dy = 0
+
+        # Get Input
+        key = pygame.key.get_pressed()
+
+        # Moving along x
+        if not self.attacking:
+            if key[pygame.K_a]:
+                dx = -MOVSPEED
+            if key[pygame.K_d]:
+                dx = MOVSPEED
+
+            # Jump
+            if self.jump == False and key[pygame.K_w]:
+                self.vel_y = -35
+                self.jump = True
+            self.vel_y += GRAV
+            dy += self.vel_y
+
+        # Check Bounds
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+        if self.rect.right + dx > screen_width:
+            dx = screen_width - self.rect.right
+        if self.rect.bottom + dy > screen_height - 50:
+            self.vel_y = 0
+            self.jump = False
+            dy = screen_height - 50 - self.rect.bottom
+            
+        # Maintain proper facing
+        if target.rect.centerx < self.rect.centerx:
+            self.leftFacing = True
+        else:
+            self.leftFacing = False
+
+        # Update Position
+        self.rect.x += dx
+        self.rect.y += dy
+
+        # Attacking
+        if key[pygame.K_q] or key[pygame.K_e]:
+            self.attack(surface, target)
+            if key[pygame.K_q]:
+                self.attack_type = 1
+            if key[pygame.K_e]:
+                self.attack_type = 2
+
+    def attack(self, surface, target):
+        self.attacking = True
+        hitbox = pygame.Rect(self.rect.centerx - (2 * self.leftFacing * self.rect.width), self.rect.y, 2 * self.rect.width, self.rect.height)
+        pygame.draw.rect(surface, '#0011ff', hitbox)
+        if hitbox.colliderect(target.rect):
+            target.health -= 10
+        self.attacking = False
+
     def draw(self, surface):
-        pygame.draw.rect(surface, (255, 0, 0),  )
+        pygame.draw.rect(surface, '#ff0000', self.rect)
--- main.py
+++ main.py
@@ -12,20 +12,43 @@ pygame.display.set_caption("Fight!")
 
 background = pygame.image.load("assets\stages\stage ctown.png").convert_alpha()
 
+# Set FPS
+clock = pygame.time.Clock()
+FPS = 60
+
 # Functions
 def drawBackground():
     scaled_background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
     screen.blit(scaled_background, (0, 0))
 
-player1 = Character(200, 310)
-player2 = Character(700, 310)
+def drawHealth(health, x, y):
+    hpRatio = health / 100
+    pygame.draw.rect(screen, '#000000', (x - 3, y - 3, 406, 36))
+    pygame.draw.rect(screen, '#ffff00', (x, y, hpRatio * 400, 30))  
+
+
+
+player1 = Character(200, 370)
+player2 = Character(800, 370)
 
 # Loop
 run = True
 while run:
-
-    # Make Background
+    clock.tick(FPS)
+    
+    # Drawing Stage
     drawBackground()
+
+    # Drawing Stats
+    drawHealth(player1.health, 20, 20)
+    drawHealth(player2.health, SCREEN_WIDTH - 420, 20)
+
+    # Movement
+    player1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, player2)
+
+    # Draw Fighers
+    player1.draw(screen)
+    player2.draw(screen)
 
     # Event Handler
     for event in pygame.event.get():
